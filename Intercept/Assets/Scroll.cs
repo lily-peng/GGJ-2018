e3411dc087c9d67c4c9f93a738985b5f3f5a06a0
Lily Peng <ms.lilypeng@gmail.com>
2018-02-05 16:23:30 -0600

    Scrollbar overhaul

---
+++ Intercept/Assets/Scroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : MonoBehaviour {
+
+	public ScrollRect myScrollRect;
+	public Scrollbar newScrollBar;
+
+	public void Start()
+	{
+		//Change the current vertical scroll position.
+		myScrollRect.verticalNormalizedPosition = 1f;
+	}
+
+
+}
